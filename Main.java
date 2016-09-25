68a603721af837f158e0e4c8b7d9a641e33df177
Darya <daria_domashenko@mail.ru>
2016-09-25 17:27:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import org.xml.sax.SAXException;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class Main
+{
+
+     public static void main(String[] args)
+     {
+         System.out.println("Соответствует ли схема XML-документу? "+ validateXMLSchema("web.xsd", "web.xml"));
+     }
+     public static boolean validateXMLSchema(String xsdPath, String xmlPath)
+             //Вернет true в случае соответствия и false в противном.
+     {
+        try
+        {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(xsdPath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(xmlPath)));
+        }
+        catch (IOException | SAXException e)
+        {
+            System.out.println("Exception: "+e.getMessage());
+            return false;
+        }
+        return true;
+     }
+}
